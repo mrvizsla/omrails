ebf24c429a56f5d7d3d168135c68d7e74566ff18
tyler c <tylerclifton@gmail.com>
2013-08-03 12:42:51 -0500

    Add user show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Omrails::Application.routes.draw do
+  get "users/show"
+  
   resources :pins
 
   devise_for :users
+  get 'users/:id' => 'users#show', as: :user # why did I add / how does ', as: user' work?
+
   get 'about' => 'pages#about'
 
   root 'pins#index' #'pages#home'
